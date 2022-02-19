b2ff41901b6e40b9da432f64c303bc3543faafd3
sskanishk <kanishkmalviya78@gmail.com>
2022-02-19 22:24:44 +0530

    static export

--- miscellaneous/pages/pets.js
+++ miscellaneous/pages/pets.js
@@ -1,15 +1,15 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import pet1 from '../public/pets/pet1.jpg'
 
 function Pets() {
   return (
     <div>
-      <Image src={pet1} placeholder='blur' alt='pet' width='280' height='460'/>
+      <img src={pet1} placeholder='blur' alt='pet' width='280' height='460'/>
       {
         ['1','2','3','4','5'].map((i) => {
           return (
             <div key={i}>
-              <Image src={`/pets/pet${i}.jpg`} alt='pet' width='280' height='460'/>
+              <img src={`/pets/pet${i}.jpg`} alt='pet' width='280' height='460'/>
             </div>
           )
         })
