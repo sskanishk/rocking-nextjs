ee20a071da3b64c731690aea79e9b6bbb989fc43
sskanishk <kanishkmalviya78@gmail.com>
2022-02-20 08:49:17 +0530

    path matching redirect"

--- miscellaneous/next.config.js
+++ miscellaneous/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // redirect some page to other whiole they are on fixing work. 
+  redirects: async () => {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true //308   if false // 301 required for crawler
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
