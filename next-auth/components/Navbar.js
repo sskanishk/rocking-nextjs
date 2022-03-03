47effc2c14d0bc49e353376810f0db5e599c2145
sskanishk <kanishkmalviya78@gmail.com>
2022-03-03 11:28:31 +0530

    -

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -1,31 +1,37 @@
 import Link from 'next/link'
-import { signIn, signOut, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Navbar() {
-  const [session, loading] = useSession()
+  const { data: session, status } = useSession()
+  console.log('session ===> ', session)
+  console.log('status ===> ', status)
   return (
     <nav className='header'>
       <h1 className='logo'>
         <a href='#'>NextAuth</a>
       </h1>
-      <ul className={`main-nav ${!session && loading ? 'loading' : 'loaded'}`}>
+      <ul className={`main-nav ${!session && status==='loading' ? 'loading' : 'loaded'}`}>
         <li>
           <Link href='/'>
             <a>Home</a>
           </Link>
         </li>
-        <li>
-          <Link href='/dashboard'>
-            <a>Dashboard</a>
-          </Link>
-        </li>
+        {
+          session && status !== 'unauthenticated' && (
+            <li>
+              <Link href='/dashboard'>
+                <a>Dashboard</a>
+              </Link>
+            </li>
+          )
+        }
         <li>
           <Link href='/blog'>
             <a>Blog</a>
           </Link>
         </li>
 
-        {!loading && !session && (
+        {!session && status !== 'authenticated' && (
           <li>
             <Link href='/api/auth/signin'>
               <a
@@ -38,7 +44,7 @@ function Navbar() {
             </Link>
           </li>
         )}
-        {session && (
+        {session && status !== 'unauthenticated' && (
           <li>
             <Link href='/api/auth/signout'>
               <a
--- next-auth/pages/_app.js
+++ next-auth/pages/_app.js
@@ -1,7 +1,17 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import '../components/Navbar.css'
+import { SessionProvider } from "next-auth/react"; 
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
